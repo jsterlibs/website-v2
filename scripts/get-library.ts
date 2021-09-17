13b98bbb7d7d9e891ac20aad74d6a0137c832d10
Juho Vepsäläinen <bebraw@gmail.com>
2021-09-17 11:08:41 +0300

    feat: Add a script to get library data

---
+++ scripts/get-library.ts
@@ -0,0 +1,31 @@
+import { cheerio } from "cheerio";
+
+const getLibrary = async (libraryName: string) => {
+  const res = await fetch("https://jster.net/library/" + libraryName);
+  const html = await res.text();
+  const $ = cheerio.load(html);
+  const tags: string[] = [];
+
+  $(".tags a").each(function (_, e) {
+    const tag = $(e).text();
+
+    tag && tags.push(tag);
+  });
+
+  return {
+    description: $(".desc").text(),
+    logo: $(".full_logo img").attr("src"),
+    name: $("h1").text(),
+    links: {
+      site: $(".icon-home").next().attr("href"),
+      github: $(".icon-github").next().attr("href"),
+    },
+    tags,
+  };
+};
+
+if (import.meta.main) {
+  console.log(await getLibrary("jquery"));
+}
+
+export { getLibrary };
