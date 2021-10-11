d7e38ff10e7eb112108b1e277254faeac7c81698
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-11 19:01:17 +0300

    refactor: Clean up meta handling

--- src/getPageRenderer.ts
+++ src/getPageRenderer.ts
@@ -42,11 +42,16 @@ function getPageRenderer(
         const styleTag = getStyleTag(stylesheet);
 
         return htmlTemplate({
-          siteMeta,
           meta: {
             ...applyData(meta, { ...pageData, pathname, pagePath }),
             pathname,
             pagePath,
+            "og:site_name": siteMeta.siteName || "",
+            "twitter:site": siteMeta.siteName || "",
+            "og:title": meta.title || "",
+            "twitter:title": meta.title || "",
+            "og:description": meta.description || "",
+            "twitter:description": meta.description || "",
           },
           head: styleTag,
           body,
@@ -71,32 +76,20 @@ function applyData(meta: Meta, dataContext?: DataContext) {
   return ret;
 }
 
-function htmlTemplate({ siteMeta, meta, head, body, mode, page }: {
-  siteMeta: SiteMeta;
+function htmlTemplate({ meta, head, body, mode, page }: {
   meta: Meta;
   head?: string;
   body?: string;
   mode: Mode;
   page: Page;
 }) {
-  const siteName = siteMeta.siteName || "";
   const title = meta.title || "";
-  const description = meta.description || "";
-  const keywords = meta.keywords || "";
 
   return `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8"
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="${description}" />
-    <meta name="keywords" content="${keywords}" />
-    <meta property="og:site_name" content="${siteName}" />
-    <meta property="og:title" content="${title}" />
-    <meta property="og:description" content="${description}" />
-    <meta property="twitter:title" content="${title}" />
-    <meta property="twitter:description" content="${description}" />
-    <meta property="twitter:site" content="${siteName}" />
     <title>${title}</title>
     <script type="text/javascript" src="https://unpkg.com/sidewind@3.3.3/dist/sidewind.umd.production.min.js"></script>
     ${
