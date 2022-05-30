05019986025f723eec1a30c03737b3607ff41661
Juho Vepsäläinen <bebraw@gmail.com>
2022-05-30 13:02:18 +0300

    fix: Resolve tag data to enhanced library data

--- site/dataSources/tags.ts
+++ site/dataSources/tags.ts
@@ -1,13 +1,19 @@
 import type { Category } from "../../types.ts";
 import { dir, getJson } from "../../scripts/utils.ts";
+import getLibraries from "./libraries.ts";
 
 async function getTags() {
+  const libraries = await getLibraries();
+
   return Promise.all((await dir("assets/data/tags")).map(async (
     { name, path },
   ) => ({
     id: name.split(".").slice(0, -1).join(),
     title: name.split(".").slice(0, -1).join(),
-    libraries: await getJson<Category[]>(path),
+    libraries: (await getJson<Category[]>(path)).map((c) => ({
+      ...c,
+      library: libraries.find((l) => l.id === c.library.id),
+    })),
   })));
 }
 
