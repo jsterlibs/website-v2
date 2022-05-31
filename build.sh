27ff6c98c515444405f83d34dcc269bb9431636d
Juho Vepsäläinen <bebraw@gmail.com>
2022-05-31 17:27:34 +0300

    fix: Mute visibleIf errors

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.22.0
 /opt/buildhome/.deno/bin/deno task decompress:cache
-/opt/buildhome/.deno/bin/deno run -A --unstable --no-check https://deno.land/x/gustwind@v0.24.4/cli.ts -b
+/opt/buildhome/.deno/bin/deno run -A --unstable --no-check https://deno.land/x/gustwind@v0.24.5/cli.ts -b
