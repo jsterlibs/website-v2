1f424eb6423a0c07f54f88c97bb2a78434f22846
Juho Vepsäläinen <bebraw@gmail.com>
2022-05-31 16:33:13 +0300

    fix: Install gustwind before deploying

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.22.0
+/opt/buildhome/.deno/bin/deno task install:gustwind
 /opt/buildhome/.deno/bin/deno task decompress:cache
 /opt/buildhome/.deno/bin/deno task build
