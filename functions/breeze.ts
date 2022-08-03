49ffb266613434d8f7b8c21dd113430f4c4dcd53
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-03 17:04:48 +0300

    feat: Render a layout in worker

--- functions/breeze.ts
+++ functions/breeze.ts
@@ -1,25 +1,15 @@
 import breeze from "breezewind";
 import * as breezeExtensions from "breezewind/extensions";
+import * as components from "../components";
+import component from "../site/layouts/libraryPage.json";
 
-// https://github.com/cloudflare/workers-types
-export async function onRequest(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
-
-  // TODO: Expand this and render a real layout with content here
+export async function onRequest() {
+  // TODO: Set up styling (twind)
+  // TODO: Pass proper context here
   const html = await breeze({
-    component: {
-      element: "div",
-      children: "hello world",
-    },
-    components: {},
+    component,
+    // @ts-ignore: Ignore the type for now
+    components,
     context: {},
     extensions: [
       breezeExtensions.classShortcut,
