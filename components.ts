e9693b722c3e4b0f92a66d6180366f8ccae337af
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-08 20:44:00 +0300

    chore: Commit components index for CF

---
+++ components.ts
@@ -0,0 +1,15 @@
+export { default as AllCategories } from './site/components/AllCategories.json';
+export { default as AllLibraries } from './site/components/AllLibraries.json';
+export { default as BaseLayout } from './site/components/BaseLayout.json';
+export { default as BlogPosts } from './site/components/BlogPosts.json';
+export { default as H1 } from './site/components/H1.json';
+export { default as Link } from './site/components/Link.json';
+export { default as MainFooter } from './site/components/MainFooter.json';
+export { default as MainNavigation } from './site/components/MainNavigation.json';
+export { default as MetaFields } from './site/components/MetaFields.json';
+export { default as Navigation } from './site/components/Navigation.json';
+export { default as Scripts } from './site/components/Scripts.json';
+export { default as SimpleTags } from './site/components/SimpleTags.json';
+export { default as Subscribe } from './site/components/Subscribe.json';
+export { default as Tags } from './site/components/Tags.json';
+
