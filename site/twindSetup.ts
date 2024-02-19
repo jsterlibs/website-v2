7cdc7344d169a9ee7fa749dfd15697169b29d72f
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-19 15:13:28 +0200

    fix: Fix styling

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -1,9 +1,23 @@
-import * as twindColors from "https://cdn.skypack.dev/twind@0.16.16/colors?min";
-import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.5";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.1";
+import presetTypography from "https://esm.sh/@twind/preset-typography@1.0.5";
+import meta from "./meta.json" assert { type: "json" };
 
 export default {
-  theme: { extend: { colors: twindColors } },
-  plugins: {
-    ...twindTypography(),
+  presets: [presetAutoprefix(), presetTailwind(), presetTypography()],
+  rules: [
+    ["btn", "font-bold py-2 px-4 rounded"],
+    ["btn-blue", "bg-blue-500 hover:bg-blue-700 text-white"],
+    ["btn-muted", "font-light text-gray-500"],
+    // https://twind.style/rules#static-rules
+    ["mask-text", { color: "transparent", textShadow: "0 0 black" }],
+    // For navigation
+    ["pointer-events-all", { pointerEvents: "all" }],
+  ],
+  theme: {
+    extend: {
+      colors: meta.colors,
+    },
   },
+  hash: false,
 };
