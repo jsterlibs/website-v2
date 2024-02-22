702500a22f48bec1cc7a617a0242b1bd47e2a588
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-22 14:57:30 +0200

    feat: Pull library data from github

--- functions/library/[name].ts
+++ functions/library/[name].ts
@@ -1,9 +1,23 @@
-export function onRequest(
+export async function onRequest(
   context: ExecutionContext & { params: { name?: string } },
-): Response {
-  if (!context.params?.name) {
+): Promise<Response> {
+  const { name } = context.params;
+
+  if (!name) {
     return new Response("Not found", { status: 404 });
   }
 
-  return new Response(`Hello, ${context.params.name}!`);
+  const url =
+    `https://raw.githubusercontent.com/jsterlibs/website-v2/main/data/libraries/${name}.json`;
+
+  try {
+    // 1. Get library data somehow (fetch from the site itself or from github?)
+    const data = await fetch(url).then((res) => res.json());
+
+    // 2. TODO: Render through template
+    // 3. TODO: Later - add data from GitHub and other sources + cache for a day
+    return new Response(`Hello, ${JSON.stringify(data, null, 2)}!`);
+  } catch (error) {}
+
+  return new Response("Not found", { status: 404 });
 }
