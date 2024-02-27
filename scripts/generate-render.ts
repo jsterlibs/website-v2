1b8687858a6532952e52978430cfb2d8de30c642
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-27 10:14:17 +0200

    refactor: Try to integrate gustwind

--- scripts/generate-render.ts
+++ scripts/generate-render.ts
@@ -1,5 +1,5 @@
 import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
-import { dir } from "./utils.ts";
+// import { dir } from "./utils.ts";
 
 // TODO: Generate a file containing components and layouts as strings + include references to their server-side logic to bundle
 generateRenderLayout();
@@ -7,8 +7,8 @@ generateRenderLayout();
 async function generateRenderLayout() {
   const siteRoot = path.join(Deno.cwd(), "site");
 
-  const layouts = await dir(path.join(siteRoot, "layouts"));
-  const components = await dir(path.join(siteRoot, "components"));
+  // const layouts = await dir(path.join(siteRoot, "layouts"));
+  // const components = await dir(path.join(siteRoot, "components"));
 
   // TODO: Pull data from html files
   // console.log(layouts);
@@ -17,40 +17,13 @@ async function generateRenderLayout() {
   await Deno.writeTextFile("render.ts", generateCode());
 }
 
-// TODO: Maybe the real solution to this problem is to allow Gustwind plugin system
-// to be run directly in Node!
 function generateCode() {
   return `// IMPORTANT! This code has been generated, do not alter it directly
-import { htmlToBreezewind } from 'htmlisp';
-import breezewind from 'breezewind';
-import breezewindExtensions from 'breezewind';
-// TODO: Import and initialize twind
-
-// TODO: Include global utilities
-const globalUtilities = {};
-
-// TODO: Include component utilities (set up imports per component)
-const componentUtilities = {};
-
-// TODO: Include layouts
-const layouts = {};
-
-// TODO: Include components
-const components = {};
+import { build } from "gustwind";
+import plugins from "./plugins.json";
 
 function render(layoutName: string, context: Record<string, unknown>) {
-  // TODO: Initialize rendering logic
-  return breezewind({
-    component: layouts[layoutName],
-    components: { ...layouts, ...components },
-    extensions: [
-      breezewindExtensions.visibleIf,
-      breezewindExtensions.foreach,
-    ],
-    context,
-    globalUtilities,
-    componentUtilities,
-  });
+  console.log(build, plugins, layoutName, context);
 }
 
 export { render };`;
