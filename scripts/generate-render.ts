53627c521696da1cc79afbc83419508a5b6cca90
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-28 15:18:03 +0200

    chore: Sketch out static plugin loading

--- scripts/generate-render.ts
+++ scripts/generate-render.ts
@@ -1,21 +1,7 @@
-import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
-// import { dir } from "./utils.ts";
+// TODO: Prettier doesn't know what to do with "with"
+import pluginDefinition from "../plugins.json" with { type: 'json'};
 
-// TODO: Generate a file containing components and layouts as strings + include references to their server-side logic to bundle
-generateRenderLayout();
-
-async function generateRenderLayout() {
-  const siteRoot = path.join(Deno.cwd(), "site");
-
-  // const layouts = await dir(path.join(siteRoot, "layouts"));
-  // const components = await dir(path.join(siteRoot, "components"));
-
-  // TODO: Pull data from html files
-  // console.log(layouts);
-  // console.log(components);
-
-  await Deno.writeTextFile("render.ts", generateCode());
-}
+await Deno.writeTextFile("render.ts", generateCode());
 
 // TODO: 1. Generate plugin imports here in a static form and pass them to build
 function generateCode() {
@@ -23,15 +9,28 @@ function generateCode() {
 import { build } from "gustwind";
 import { plugin } from "gustwind/plugins/meta/mod.ts";
 import { initLoadApi } from "./utils/loadApi.ts";
-import plugins from "./plugins.json";
+import pluginDefinition from "./plugins.json";
+${pluginDefinition.plugins
+  .map(
+    (plugin, i) =>
+      `import p${i} from "` +
+      plugin.path.replace("${GUSTWIND_VERSION}", "gustwind") +
+      `";`
+  )
+  .join(`\n`)}
+const plugins = [${pluginDefinition.plugins.map((_, i) => `p${i}`).join(", ")}];
 
-plugins.env.GUSTWIND_VERSION = "gustwind";
+// @ts-expect-error This is fine for now
+pluginDefinition.plugins = pluginDefinition.plugins.map((p, i) => ({
+  module: plugins[i].plugin,
+  options: p.options,
+}));
 
 function render(layoutName: string, context: Record<string, unknown>) {
   console.log('plugin', plugin);
-  console.log(build, plugins, layoutName, context);
+  console.log(build, pluginDefinition, layoutName, context);
 
-  return build("./", initLoadApi, plugins, "blog");
+  return build("./", initLoadApi, pluginDefinition, "blog");
 }
 
 export { render };`;
