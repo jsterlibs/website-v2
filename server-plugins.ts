b90ff22be314b22d74a87c693562e483b3223955
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-29 15:54:00 +0200

    chore: Set up import maps for twind setup

--- server-plugins.ts
+++ server-plugins.ts
@@ -2,7 +2,7 @@ import { plugin as htmlispEdgePlugin } from "gustwind/plugins/htmlisp-edge-rende
 import { plugin as metaPlugin } from "gustwind/plugins/meta";
 import { plugin as twindPlugin } from "gustwind/plugins/twind";
 import * as globalUtilities from "./site/globalUtilities";
-import twindSetup from "./site/twindSetup.node";
+import twindSetup from "./site/twindSetup";
 import meta from "./site/meta.json";
 
 // The manifest is a generated file since the edge relies on static imports which
