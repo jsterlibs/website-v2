7db49045986671bea5de980c0a191ae4c2167643
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-29 16:03:30 +0200

    chore: Set up import maps for markdown

--- site/transforms/markdown.ts
+++ site/transforms/markdown.ts
@@ -1,14 +1,13 @@
-import { Html5Entities } from "https://deno.land/x/html_entities@v1.0/mod.js";
-import { install, tw } from "https://esm.sh/@twind/core@1.1.1";
-import { marked } from "https://unpkg.com/marked@9.1.5/lib/marked.esm.js";
-import highlight from "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/core.min.js";
-import highlightBash from "https://unpkg.com/highlight.js@11.9.0/es/languages/bash";
-import highlightJS from "https://unpkg.com/highlight.js@11.9.0/es/languages/javascript";
-import highlightJSON from "https://unpkg.com/highlight.js@11.9.0/es/languages/json";
-import highlightTS from "https://unpkg.com/highlight.js@11.9.0/es/languages/typescript";
-import highlightYAML from "https://unpkg.com/highlight.js@11.9.0/es/languages/yaml";
-import twindSetup from "../twindSetup.ts";
-import type { LoadApi } from "https://deno.land/x/gustwind@v0.52.3/types.ts";
+import * as Html5Entities from "../utilities/html5entities";
+import { install, tw } from "@twind/core";
+import { marked } from "marked";
+import highlight from "highlight.js";
+import highlightBash from "highlight.js/lib/languages/bash";
+import highlightJS from "highlight.js/lib/languages/javascript";
+import highlightJSON from "highlight.js/lib/languages/json";
+import highlightTS from "highlight.js/lib/languages/typescript";
+import highlightYAML from "highlight.js/lib/languages/yaml";
+import twindSetup from "../twindSetup";
 
 highlight.registerLanguage("bash", highlightBash);
 highlight.registerLanguage("javascript", highlightJS);
@@ -32,7 +31,7 @@ marked.setOptions({
 
 install(twindSetup);
 
-function getTransformMarkdown(load: LoadApi) {
+function getTransformMarkdown() {
   return function transformMarkdown(input: string) {
     // https://github.com/markedjs/marked/issues/545
     const tableOfContents: { slug: string; level: number; text: string }[] = [];
@@ -64,12 +63,11 @@ function getTransformMarkdown(load: LoadApi) {
           code = code.replace(/\n$/, "") + "\n";
 
           if (!lang) {
-            return "<pre><code>" +
-              code +
-              "</code></pre>\n";
+            return "<pre><code>" + code + "</code></pre>\n";
           }
 
-          return '<pre class="' +
+          return (
+            '<pre class="' +
             tw`overflow-auto -mx-4 md:mx-0 bg-gray-100` +
             '"><code class="' +
             // @ts-ignore How to type this?
@@ -77,20 +75,22 @@ function getTransformMarkdown(load: LoadApi) {
             lang +
             '">' +
             code +
-            "</code></pre>\n";
+            "</code></pre>\n"
+          );
         },
-        heading(
-          text: string,
-          level: number,
-          raw: string,
-        ) {
+        heading(text: string, level: number, raw: string) {
           const slug = slugify(raw);
 
           tableOfContents.push({ slug, level, text });
 
-          return '<a href="#' + slug + '"><h' +
+          return (
+            '<a href="#' +
+            slug +
+            '"><h' +
             level +
-            ' class="' + tw`inline` + '"' +
+            ' class="' +
+            tw`inline` +
+            '"' +
             ' id="' +
             slug +
             '">' +
@@ -98,7 +98,8 @@ function getTransformMarkdown(load: LoadApi) {
             "</h" +
             level +
             ">" +
-            "</a>\n";
+            "</a>\n"
+          );
         },
         image(href: string, title: string, text: string) {
           const textParts = text ? text.split("|") : [];
@@ -107,9 +108,9 @@ function getTransformMarkdown(load: LoadApi) {
           const height = textParts[2] || "";
           const className = textParts[3] || "";
 
-          return `<img src="${href}" alt="${alt}" class="${
-            tw(className)
-          }" width="${width}" height="${height}" />`;
+          return `<img src="${href}" alt="${alt}" class="${tw(
+            className
+          )}" width="${width}" height="${height}" />`;
         },
         link(href: string, title: string, text: string) {
           if (href === null) {
@@ -122,8 +123,12 @@ function getTransformMarkdown(load: LoadApi) {
 
           const parts = text.split("|");
 
-          let out = '<a class="' + tw(["underline"].concat(parts[1])) +
-            '" href="' + href + '"';
+          let out =
+            '<a class="' +
+            tw(["underline"].concat(parts[1])) +
+            '" href="' +
+            href +
+            '"';
           if (title) {
             out += ' title="' + title + '"';
           }
@@ -132,16 +137,22 @@ function getTransformMarkdown(load: LoadApi) {
         },
         list(body: string, ordered: string, start: number) {
           const type = ordered ? "ol" : "ul",
-            startatt = (ordered && start !== 1)
-              ? (' start="' + start + '"')
-              : "",
+            startatt = ordered && start !== 1 ? ' start="' + start + '"' : "",
             klass = ordered
               ? "list-decimal list-inside"
               : "list-disc list-inside";
-          return "<" + type + startatt + ' class="' + tw(klass) + '">\n' +
+          return (
+            "<" +
+            type +
+            startatt +
+            ' class="' +
+            tw(klass) +
+            '">\n' +
             body +
             "</" +
-            type + ">\n";
+            type +
+            ">\n"
+          );
         },
       },
     });
