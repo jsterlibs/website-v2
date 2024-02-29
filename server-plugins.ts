beb917b2fdcd9624980578aa4c97a4a2850e1da6
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-29 15:35:03 +0200

    chore: Update gustwind

--- server-plugins.ts
+++ server-plugins.ts
@@ -1,16 +1,16 @@
-// TODO: Change export map
-import { plugin as breezewindPlugin } from "gustwind/plugins/breezewind-renderer";
+import { plugin as htmlispEdgePlugin } from "gustwind/plugins/htmlisp-edge-renderer";
 import { plugin as metaPlugin } from "gustwind/plugins/meta";
 import { plugin as twindPlugin } from "gustwind/plugins/twind";
 import * as globalUtilities from "./site/globalUtilities";
 import twindSetup from "./site/twindSetup";
 import meta from "./site/meta.json";
+
+// The manifest is a generated file since the edge relies on static imports which
+// complicates things a little.
 import { components, componentUtilities } from "./manifest";
 
 export default [
-  // TODO: Pass loaded components and globalUtilities to renderer
-  // TODO: Likely this has to handle loading server.ts files related to each component as well
-  breezewindPlugin.init({ components, componentUtilities, globalUtilities }),
+  htmlispEdgePlugin.init({ components, componentUtilities, globalUtilities }),
   metaPlugin.init({ meta }),
   twindPlugin.init({ twindSetup }),
 ];
