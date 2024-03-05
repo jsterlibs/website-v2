1d4a3f3a2fbb0330ee1e38bf78ef63ec1a242209
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-05 11:20:35 +0200

    fix: Generate manifest on build

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.40.5
 /opt/buildhome/.deno/bin/deno task decompress:cache
 /opt/buildhome/.deno/bin/deno task build
+/opt/buildhome/.deno/bin/deno task generate:manifest
