f5ab8b61274827040d9a81a0d936b68c82d32d1d
Juho Vepsäläinen <bebraw@gmail.com>
2024-03-06 10:15:31 +0200

    fix: Access env in a correct way

--- functions/library/[name].ts
+++ functions/library/[name].ts
@@ -1,12 +1,13 @@
 import { initRender } from "../../render.ts";
 
+type Env = { API_AUTH: string };
+
 const ONE_HOUR = 60 * 60;
 const ONE_DAY = ONE_HOUR * 24;
 
 // Reference: https://developers.cloudflare.com/workers/examples/cache-using-fetch/
 export async function onRequest(
-  context: ExecutionContext & { params: { name?: string } },
-  env: { API_AUTH: string }
+  context: ExecutionContext & { params: { name?: string }; env: Env }
 ): Promise<Response> {
   const { name } = context.params;
 
@@ -14,7 +15,7 @@ export async function onRequest(
     return new Response("Not found", { status: 404 });
   }
 
-  console.log("env", env);
+  console.log("env", context.env.API_AUTH);
 
   const url = `https://raw.githubusercontent.com/jsterlibs/website-v2/main/data/libraries/${name}.json`;
 
